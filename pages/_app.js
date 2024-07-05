e039e6872667fb3b58dd32586c91c6076141ee93
Alan Dias <jusbrasil@JUS-202300505.local>
2024-07-05 09:47:23 -0300

    fix: Adapt GA script to lattest Next patterns

--- pages/_app.js
+++ pages/_app.js
@@ -1,19 +1,16 @@
 import "@styles/globals.scss";
 import Head from 'next/head';
+
+import { GoogleAnalytics } from '@next/third-parties/google';
+
 export default function App({ Component, pageProps }) {
     return (
     <>
       <Head>
         <script async src="https://tally.so/widgets/embed.js"></script>
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7E46RP1M4D"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments)}
-          gtag('js', new Date());
-          gtag('config', 'G-7E46RP1M4D');
-        </script>
       </Head>
       <Component {...pageProps} />
+      <GoogleAnalytics gaId="G-7E46RP1M4D"/>
     </>
   )
 }
