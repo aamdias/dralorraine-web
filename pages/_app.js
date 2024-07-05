2cb0dca864842e55dcfaefb9b3e89a239c32a309
Alan Dias <jusbrasil@JUS-202300505.local>
2024-07-05 09:42:14 -0300

    feat: Add GA script

--- pages/_app.js
+++ pages/_app.js
@@ -5,6 +5,13 @@ export default function App({ Component, pageProps }) {
     <>
       <Head>
         <script async src="https://tally.so/widgets/embed.js"></script>
+        <script async src="https://www.googletagmanager.com/gtag/js?id=G-7E46RP1M4D"></script>
+        <script>
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments)}
+          gtag('js', new Date());
+          gtag('config', 'G-7E46RP1M4D');
+        </script>
       </Head>
       <Component {...pageProps} />
     </>
